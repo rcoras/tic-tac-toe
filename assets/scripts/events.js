4e67e0f430d3549ad99d42f3dd9599c3764d4f2f
rcoras <rcoras@yahoo.com>
2017-12-21 16:30:20 -0500

    Clicking box rotates between adding an X and O to square
    Add code box will add X for the first click, and O for the second click, switching back and forth throughout the game. Need to add logic to disallow user from clicking on box already clicked

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,13 +1,16 @@
 'use strict'
+let playerToken = 'X'
 
 const onBoxClick = function (event) {
+  // create boxId token to use in jquery to update correct box
   const boxId = '#' + this.id
-  $(boxId).text('X')
-  // let playerToken = 'X'
-  // if (playerToken === 'X') {
-  //   playerToken = 'O'
-  // } else {
-  //   playerToken = 'X'
+  if (playerToken === 'X') {
+    playerToken = 'O'
+    $(boxId).text('X')
+  } else {
+    playerToken = 'X'
+    $(boxId).text('O')
+  }
 }
 
 const addHandlers = function () {
