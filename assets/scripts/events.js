f525eb4baf6bc3ab4337e4a7f75a9ed325659c80
rcoras <rcoras@yahoo.com>
2017-12-28 09:34:52 -0500

    remove console.log 'did it work' from signout function
    remove console.log that was added to troubleshoot

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -85,7 +85,6 @@ const onChangePassword = function (event) {
 
 const onSignOut = function (event) {
   event.preventDefault()
-  console.log('did this work?')
   api.signOut()
     .then(ui.signOutSuccess)
     .catch(ui.signOutFailure)
