2a0d96cb80b5e32bb25a839c18f0b0e0a2a122bf
rcoras <rcoras@yahoo.com>
2018-01-02 11:45:54 -0500

    Remove sign up and sign in button once action is taken. Add messaging to show when action is taken
    Sign up and sign in button are removed from the screen once they are completed successfully
    Messaging is added to the screen when user signs up, in, changes pw, and signs out

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -4,11 +4,14 @@ const store = require('./store')
 const signUpSuccess = function (data) {
   console.log(data)
   console.log('successful sign up')
+  $('#closeSignUpButton').hide()
+  $('#signInMessaging').text('You\'ve successfully signed up! Log in to start playing!')
 }
 
 const signUpFailure = function (error) {
   console.error(error)
   console.log('this didn\'t work')
+  $('#signInMessaging').text('Uh Oh That didn\'t work, try again')
 }
 
 const signInSuccess = function (data) {
@@ -16,29 +19,37 @@ const signInSuccess = function (data) {
   console.log('Successfully signed in!')
   // we use the code below to save the user data to use the token and id
   store.user = data.user
+  $('#signInMessaging').text('You\'ve successfully signed in! Press start playing to start!')
+  $('#closeSignInButton').hide()
+  $('#showChangePwButton').show()
+  $('#showSignOut').show()
 }
 
 const signInFailure = function (error) {
   console.error(error)
   console.log('Error on sign in')
+  $('#signInMessaging').text('Uh Oh That didn\'t work, try again')
 }
 
 const changePasswordSuccess = function (data) {
   console.log('Successfully changed password')
+  $('#signInMessaging').text('You\'re password has been updated')
 }
 
 const changePasswordFailure = function (error) {
   console.error(error)
+  $('#signInMessaging').text('Uh Oh That didn\'t work, try again')
 }
 
 const signOutSuccess = function (data) {
   console.log('Successfully signed out')
   store.user = null
+  $('#signInMessaging').text('You\'re signed out!')
 }
 
 const signOutFailure = function (error) {
   console.error(error)
-  $('#message').text('Error signing out')
+  $('#signInMessaging').text('Uh Oh That didn\'t work, try again')
 }
 
 const startGameSuccess = function (data) {
